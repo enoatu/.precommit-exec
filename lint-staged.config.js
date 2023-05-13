ef4c6c452b90b458b72ca9fc22c53c35452c6076
enoatu <acro1452@gmail.com>
2023-05-13 17:55:17 +0900

    :wrench: add examples

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,4 +3,8 @@ module.exports = {
     (filenames) => `yarn eslint --fix --config .eslintrc.yml ${filenames.join(' ')}`,
     (filenames) => `yarn prettier --write --config .prettierrc.json ${filenames.join(' ')}`,
   ],
+  // '../**/*.go': [
+  //   'cd .. && gofmt -w',
+  //   'cd .. && goimports -w'
+  // ],
 }
